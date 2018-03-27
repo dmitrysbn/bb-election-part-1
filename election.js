8ed9dc7fb3ec9b44b5e60a058dd2e72e7720b5d1
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-27 12:49:13 -0400

    Display candidates and votes

--- election.js
+++ election.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  var candidatesList = document.querySelector('#candidate-list');
+
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    data: {},
+    dataType: 'json'
+  }).done(function(responseData) {
+    responseData.candidates.forEach(function(candidate) {
+      var candidateItem = document.createElement('li');
+      var candidateParagraph = document.createElement('p');
+
+      candidateParagraph.innerText = candidate.name + ": " + candidate.votes;
+
+      candidateItem.append(candidateParagraph);
+      candidatesList.append(candidateItem);
+    });
+  });
 
 });
